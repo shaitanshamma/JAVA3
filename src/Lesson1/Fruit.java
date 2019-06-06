196d9881180f9f660621c83656c23318f411f13d
shaitanshamma <kurbatov_andr@hotmail.com>
2019-06-06 21:44:18 +0300

    Еще пара методов

--- src/Lesson1/Fruit.java
+++ src/Lesson1/Fruit.java
@@ -47,10 +47,17 @@ class Box<T extends Fruit> {
 
     public boolean compare(Box anotherBox) {
         if (getWeight(this) == getWeight(anotherBox)) {
-
             return true;
         }
         return false;
+    }
+
+
+    public void changeBox(Box<T> newFruitBox) {
+        for (int i = 0; i < this.fruitBox.size(); i++) {
+            newFruitBox.fruitBox.add(this.fruitBox.get(i));
+        }
+        fruitBox.removeAll(this.fruitBox);
     }
 }
 
@@ -59,21 +66,34 @@ class Main {
         Apple a1 = new Apple(1);
         Apple a2 = new Apple(1);
         Apple a3 = new Apple(1);
+        Apple a4 = new Apple(1);
+        Apple a5 = new Apple(1);
+        Apple a6 = new Apple(1);
         Orange o1 = new Orange(2);
         Orange o2 = new Orange(2);
         Orange o3 = new Orange(2);
         ArrayList<Apple> apples = new ArrayList<>();
+        ArrayList<Apple> apples2 = new ArrayList<>();
         ArrayList<Orange> oranges = new ArrayList<>();
         Box<Apple> appleBox = new Box<>(apples);
+        Box<Apple> appleBox2 = new Box<>(apples2);
         Box<Orange> orangeBox = new Box<>(oranges);
         appleBox.fruitBox.add(a1);
         appleBox.fruitBox.add(a2);
         appleBox.fruitBox.add(a3);
+        appleBox2.fruitBox.add(a4);
+        appleBox2.fruitBox.add(a5);
+        appleBox2.fruitBox.add(a6);
         orangeBox.fruitBox.add(o1);
         orangeBox.fruitBox.add(o2);
         orangeBox.fruitBox.add(o3);
-        System.out.println(orangeBox.getWeight(orangeBox));
+        System.out.println("Вес коробки с апельсинами = " + orangeBox.getWeight(orangeBox));
+        System.out.println("Вес коробки с яблоками = " + appleBox.getWeight(appleBox));
+        System.out.println("Вес второй коробки с яблоками = " + appleBox2.getWeight(appleBox2));
+        System.out.println("Одинаково ли весят коробки  = " + orangeBox.compare(appleBox));
+        appleBox2.changeBox(appleBox);
+        System.out.println("Вес после пересыпки =");
         System.out.println(appleBox.getWeight(appleBox));
-        System.out.println(orangeBox.compare(appleBox));
+        System.out.println(appleBox2.getWeight(appleBox2));
     }
 }
