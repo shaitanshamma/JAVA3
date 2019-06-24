cca122d6e46cefa8272b78827605c42edf50efed
shaitanshamma <kurbatov_andr@hotmail.com>
2019-06-24 20:47:36 +0300

    Пару новых тестов

--- Lesson3/src/test/java/TestArr.java
+++ Lesson3/src/test/java/TestArr.java
@@ -1,14 +1,43 @@
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 public class TestArr {
+    private Array array;
 
-
-    @Test(expected = AssertionError.class)
-        public void testArr1(){
-        Array array = new Array();
-        int [] arr = {1,2,3,0,5,2,3};
-        Assert.assertArrayEquals(new int[]{},array.checkFourInArray(arr));
+    @Before
+    public void start(){
+        array = new Array();
     }
 
+    @Test(expected = RuntimeException.class)
+    public void testArr1(){
+        int [] checkingArr = {3,2,3,0,5,2,3};
+        int [] rezultArr = {3,2,3,0,5,2};
+        Assert.assertArrayEquals(rezultArr,array.checkFourInArray(checkingArr));
+    }
 
+    @Test(expected = AssertionError.class)
+    public void testArr5(){
+        int [] checkingArr = {3,2,3,0,5,2,3};
+        int [] rezultArr = {3,2,3,0,5,2};
+        Assert.assertArrayEquals(rezultArr,array.checkFourInArray(checkingArr));
+    }
+    @Test()
+    public void testArr2(){
+        int [] checkingArr = {3,4,3,0,5,2,3};
+        int [] rezultArr = {3,0,5,2,3};
+        Assert.assertArrayEquals(rezultArr,array.checkFourInArray(checkingArr));
+    }
+    @Test()
+    public void testArr3(){
+        int [] checkingArr = {3,4,3,4,5,2,3};
+        int [] rezultArr = {3,0,5,2,3};
+        Assert.assertArrayEquals(rezultArr,array.checkFourInArray(checkingArr));
+    }
+    @Test()
+    public void testArr4(){
+        int [] checkingArr = {3,4,3,0,5,2,4,2,1,1,1,3};
+        int [] rezultArr = {2,1,1,1,3};
+        Assert.assertArrayEquals(rezultArr,array.checkFourInArray(checkingArr));
+    }
 }
