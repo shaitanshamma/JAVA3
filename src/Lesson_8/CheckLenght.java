9cafa9f959242b997f55513202d31333a3ee1f5e
shaitanshamma <kurbatov_andr@hotmail.com>
2019-06-30 22:03:24 +0300

    Второй тест

--- src/Lesson_8/CheckLenght.java
+++ src/Lesson_8/CheckLenght.java
@@ -4,21 +4,30 @@ import java.util.ArrayList;
 
 public class CheckLenght {
 
-    static Object resume = new Object();
+    static Object res = new Object();
+    static Object marker = new Object();
 
     static void push(ArrayList arrayList){
-        arrayList.add(resume);
+        arrayList.add(res);
+    }
+    static void pushMarker(ArrayList arrayList, Object o){
+        arrayList.add(res);
     }
 
     static Object pop(ArrayList arrayList){
-        return resume = arrayList.get(0);
+        return res = arrayList.get(0);
     }
+    static Object popOne(ArrayList arrayList){
+        return marker = arrayList.get(0);
+    }
+
 
 }
 class Main {
     public static void main(String[] args) {
         ArrayList<Object> list = new ArrayList<>();
         int size =0;
+        int size2 =0;
         Object a = new Object();
         Object b = new Object();
         Object c = new Object();
@@ -30,10 +39,26 @@ class Main {
         list.add(c);
         list.add(d);
         list.add(e);
+
+
+        // Первый вариант, ленивый.
+
         for (Object o:list) {
             CheckLenght.pop(list);
             size++;
         }
-        System.out.println(size);
+
+        // Второй вариант
+        CheckLenght.popOne(list);
+        System.out.println(CheckLenght.marker);
+        CheckLenght.pushMarker(list, CheckLenght.marker);
+        size2++;
+        for (Object o:list) {
+            if(!o.equals(CheckLenght.marker)){
+                size2++;
+            }
+        }
+        System.out.println("Коллекция состоит из " + size2 + " элементов.");
+        System.out.println("Коллекция состоит из " + size + " элементов.");
     }
 }
